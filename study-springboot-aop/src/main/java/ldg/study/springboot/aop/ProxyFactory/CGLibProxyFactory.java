c7f207e21b6414393192b51813d0822d1224018c
ldg <lidegui@ccuol.com>
2019-08-22 16:35:35 +0800

    cglib

--- study-springboot-aop/src/main/java/ldg/study/springboot/aop/ProxyFactory/CGLibProxyFactory.java
+++ study-springboot-aop/src/main/java/ldg/study/springboot/aop/ProxyFactory/CGLibProxyFactory.java
@@ -2,6 +2,7 @@ package ldg.study.springboot.aop.ProxyFactory;
 
 import org.aopalliance.aop.Advice;
 import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.cglib.proxy.Callback;
 import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
@@ -50,6 +51,17 @@ public class CGLibProxyFactory {
     };
 
     public Object createProxy() {
+        //输出 CGLIB 代理类文件到指定目录
+        //CGLIB 代理类文件（3个）
+        /*
+            原理：读取“被代理类”的字段、构造方法、普通方法，通过ASM（字节码操作框架）生成CGLIB 代理类，代理类 继承了“被代理类”，
+                  代理类添加了“被代理类”的所有方法作为代理类的属性 和 普通方法
+                  当通过代理对象调用普通方法时，调用的是代理类的普通方法，普通方法再通过 MethodInterceptor调用方法拦截器，然调用具体的类
+
+                  可通过访问“creageProxy”方法，查看已生成的“代理类”文件（放在D:\code目录下），通过反编译工具 查看“代理类”的字节码代码
+        */
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\code");
+
         // 1. 创建Enhancer对象
         Enhancer enhancer = new Enhancer();
 
